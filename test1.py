95d448a4d559cc030e4270461b0cb8f357d1f838
RaderH2O <raderh2ogt@gmail.com>
2020-09-14 00:33:33 +0430

    Init

---
+++ test1.py
@@ -0,0 +1,15 @@
+import PySimpleGUI as sg
+
+layout = [
+    [sg.Button("Do not click me", k="addBtn")],
+    [sg.Text("You are lucky to run this app")]
+]
+win = sg.Window("This is a window", layout)
+while True:
+    events, value = win.read()
+    if events == sg.WINDOW_CLOSED:
+        break
+    if events == "addBtn":
+        print("Why?")
+win.close()
+# Ummm
